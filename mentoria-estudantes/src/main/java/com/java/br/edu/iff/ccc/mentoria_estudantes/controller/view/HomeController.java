288dedef63d761fb5872e17fb7de8cae0a22cd40
Lucas Lorenzo Savino <savino@MacBook-Air-de-Lucas.local>
2025-09-28 22:59:13 -0300

    feat: Implement UI layout with Thymeleaf fragments

---
+++ mentoria-estudantes/src/main/java/com/java/br/edu/iff/ccc/mentoria_estudantes/controller/view/HomeController.java
@@ -0,0 +1,13 @@
+package com.java.br.edu.iff.ccc.mentoria_estudantes.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
