8735fd299ceead4cd8f31c7b8c1475fc8d7155ec
Lucas Lorenzo Savino <savino@MacBook-Air-de-Lucas.local>
2025-09-28 21:23:16 -0300

    init: Initial project structure and .gitignore

---
+++ mentoria-estudantes/src/main/java/com/java/br/edu/iff/ccc/mentoria_estudantes/MentoriaEstudantesApplication.java
@@ -0,0 +1,13 @@
+package com.java.br.edu.iff.ccc.mentoria_estudantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoriaEstudantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoriaEstudantesApplication.class, args);
+	}
+
+}
---
+++ mentoria-estudantes/src/test/java/com/java/br/edu/iff/ccc/mentoria_estudantes/MentoriaEstudantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.br.edu.iff.ccc.mentoria_estudantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoriaEstudantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
